d4c96437d85a76069f818c2acf40af6e178a5a6e
Aratrika Ray <aratrika.ray1999@gmail.com>
2020-05-06 03:44:10 +0530

    Updates

---
+++ precache-manifest.c6fcf531211847d0a44178abca890fd0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03358907b254195f0d4d6b0edb7fea98",
+    "url": "/faceDetect/index.html"
+  },
+  {
+    "revision": "e4592b42b32e1b574338",
+    "url": "/faceDetect/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "87e1ecd86cafcf97c52d",
+    "url": "/faceDetect/static/css/main.79e05b49.chunk.css"
+  },
+  {
+    "revision": "e4592b42b32e1b574338",
+    "url": "/faceDetect/static/js/2.beb4ff8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/faceDetect/static/js/2.beb4ff8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87e1ecd86cafcf97c52d",
+    "url": "/faceDetect/static/js/main.7c81faa9.chunk.js"
+  },
+  {
+    "revision": "d5f31348d94f97b82731",
+    "url": "/faceDetect/static/js/runtime-main.c2a0895f.js"
+  },
+  {
+    "revision": "f5d08743fc910b2b0d55bc551e2f2e73",
+    "url": "/faceDetect/static/media/logo.f5d08743.png"
+  }
+]);
